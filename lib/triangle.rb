b6b803dc6470345ebdff43d72628cc958fd46771
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-05-30 16:21:24 +0300

    completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
   # write code here
+
+  def initialize (a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  def kind
+    if(@a <= 0 || @b <= 0 || @c <== 0)
+      raise TriangleError
+    elsif (@a + b <= @c || @b + @c <= @ || @a + @c <= @b)
+      :equilateral
+    elsif (@a == @b || @a == @c || @b == @c)
+      :isosceles
+    else 
+      :scalene 
+  end
+end
+class TriangleError < StandardError 
+  def message 
+    "triangle error code"
+  end
+end
 end
